4bfa7bac252ec8bfc6dffff88bf4eccb057c1427
saisantosh2805 <saisantosh2000naviri@gmail.com>
2024-11-05 00:24:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ class Person {
     this.name = name;
     this.age = age;
   }
-
+ 
   greet() {
     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
   }
 }
-
+ 
 class Employee extends Person {
   constructor(name, age, jobTitle) {
-    super(name, age); // Call the parent class constructor
+    super(name, age);
     this.jobTitle = jobTitle;
   }
-
+ 
   jobGreet() {
     console.log(
-      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+      `Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`
     );
   }
 }
-const p1 = new Person("Alice",25);
+const p1 = new Person("Alice", 25);
 p1.greet();
-const Emp = new Employee("Bob",30,"Manager");
+const Emp = new Employee("Bob", 30, "Manager");
 Emp.jobGreet();
 
-// Expose the classes to the global window object for Cypress testing
+
 window.Person = Person;
 window.Employee = Employee;
