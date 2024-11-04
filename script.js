56a95dee3ff6530c61b7740600546a003bf0fac3
saisantosh2805 <saisantosh2000naviri@gmail.com>
2024-11-05 00:26:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ class Person {
   }
  
   greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
   }
 }
  
@@ -17,15 +17,27 @@ class Employee extends Person {
  
   jobGreet() {
     console.log(
-      `Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
     );
   }
 }
+ 
+// You create an instance of the Person class with the
+// name "Alice" and age 25, and call the greet method.
+// The expected console output is: Hello, my name is Alice, I am 25
+//  years old.
+ 
 const p1 = new Person("Alice", 25);
 p1.greet();
+ 
+// Next, you create an instance of the Employee class with the name "Bob",
+// age 30, and job title "Manager", and call the jobGreet method.
+// The expected console output is: Hello, my name is Bob, I am 30 years old,
+// and my job title is Manager.
+ 
 const Emp = new Employee("Bob", 30, "Manager");
 Emp.jobGreet();
-
-
+ 
+// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
